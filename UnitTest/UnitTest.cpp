ffc8909524962019ebca302f159065305a25a3a6
joechung99 <joechung99.cv05g@nctu.edu.tw>
2017-04-13 03:05:34 +0800

    boundary test

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -15,23 +15,30 @@ extern "C" {
 
 #include <iostream>
 using namespace std;
-TEST(chewingtest,combinationtest){
+TEST(chewingtest,boundarytest){
 	ChewingContext* ctx = chewing_new();
-	chewing_handle_Default(ctx, 'n');
-	chewing_handle_Default(ctx, 'u');
-	chewing_handle_Space(ctx);
+	chewing_handle_Default(ctx, '[');
 	chewing_handle_Enter(ctx);
-	EXPECT_STREQ("",chewing_commit_String(ctx));
+	EXPECT_STREQ("「",chewing_commit_String(ctx));
 }
-TEST(chewingtest,combinationtest2){
+TEST(chewingtest,boundarytest2){
+        ChewingContext* ctx = chewing_new();
+        chewing_handle_Default(ctx, ']');
+	chewing_handle_Enter(ctx);
+        EXPECT_STREQ("」",chewing_commit_String(ctx));
+}
+TEST(chewingtest,boundarytest3){
         ChewingContext* ctx = chewing_new();
         chewing_handle_Default(ctx, '[');
-        //chewing_handle_Default(ctx, '');
-        //chewing_handle_Space(ctx);
-	chewing_handle_Enter(ctx);
-        EXPECT_STRNE("「",chewing_commit_String(ctx));
+        chewing_handle_Enter(ctx);
+        EXPECT_STRNE("」",chewing_commit_String(ctx));
 }
-
+TEST(chewingtest,boundarytest4){
+        ChewingContext* ctx = chewing_new();
+        chewing_handle_Default(ctx, '=');
+        chewing_handle_Enter(ctx);
+        EXPECT_STREQ("＝",chewing_commit_String(ctx));
+}
 
 
 int main( int argc, char** argv){
@@ -39,17 +46,14 @@ int main( int argc, char** argv){
 	return RUN_ALL_TESTS();
 	ChewingContext* ctx = chewing_new();
 	//type_keystroke_by_string( ctx, "su3cl3");
-	chewing_handle_Default(ctx, '[');
-	//cout << chewing_handle_Default(ctx, 'x');
-	chewing_handle_Default(ctx, ']');
-	//cout << chewing_handle_Default(ctx, 'm');
+	chewing_handle_Default(ctx, '=');
+	//chewing_handle_Default(ctx, ']');
 	//chewing_handle_Default(ctx, '4');
 	//chewing_handle_Default(ctx, 't');
 	//chewing_handle_Default(ctx, '8');
 	//chewing_handle_Default(ctx, '6');
 	chewing_handle_Enter(ctx);
-  	cout<<chewing_commit_Check(ctx)<<endl;
-	cout << chewing_commit_String(ctx);
+	//cout << chewing_commit_String(ctx);
 	if( chewing_commit_Check(ctx)){
 		cout << chewing_commit_String(ctx);
 	}
